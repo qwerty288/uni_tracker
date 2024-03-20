247c88778162543cf642b7e5994cce54b8fa7a1f
Prasanth <anon88e@email.com>
2024-03-20 05:25:09 +0000

    Impkemented getWeeklyConnectedComponents

--- server/services/getWeaklyConnectedComponents.js
+++ server/services/getWeaklyConnectedComponents.js
@@ -1,4 +1,56 @@
 export default function getWeaklyConnectedComponents(adjacencyList) {
-    var res = []
-    return res
+    let parents = {}
+    let rank = {}
+    let res_dict = {}
+    let final_res = new Set()
+    for (const key in adjacencyList) {
+        parents[key] = key
+        rank[key] = 1
+        res_dict[key] = new Set([key])
+    }
+
+    function find(node) {
+        let res = node
+        // Loop until a root node has been found (where parent is itself)
+        while (res != parents[res]) {
+            res = parents[res]
+        }
+        return res
+    }
+
+    function union(u, v) {
+        let parent1 = find(u)
+        let parent2 = find(v)
+        // If both parents are the same, this indicates u and v belong in the same component
+        // If both parents aren't the same, then perform union
+        if (rank[parent2] > rank[parent1]) {
+            parents[parent1] = parent2
+            rank[parent2] += rank[parent1]
+            res_dict[parent2].add(u)
+
+        } else {
+            parents[parent2] = parent1
+            rank[parent1] += rank[parent2]
+            res_dict[parent1].add(v)
+        }
+    }
+
+    // Perform union on all edges in adjacency list    
+    for (const u in adjacencyList) {
+        for (var i = 0; i < adjacencyList[u].length; i++) {
+            let v = adjacencyList[u][i]
+            union(u, v)
+        }
+    }
+
+    // Create sets of components from the trees
+    for (const key in res_dict) {
+        // If a node parent is itself, then it is a tree root
+        if (parents[key] == key) {
+            final_res.add(res_dict[key])
+        }
+    }
+
+    return final_res
 }
+
--- server/tests/getWeaklyConnectedComponents.test.js
+++ server/tests/getWeaklyConnectedComponents.test.js
@@ -2,35 +2,132 @@ import getWeaklyConnectedComponents from '../services/getWeaklyConnectedComponen
 
 
 test('Base case', () => {
-    expect(getWeaklyConnectedComponents({})).toStrictEqual([])
+    expect(getWeaklyConnectedComponents({})).toStrictEqual(
+      new Set([])
+      )
   })
 
   test('Single node graph', () => {
     expect(getWeaklyConnectedComponents({
       1: []
-    })).toStrictEqual([])
+    })).toStrictEqual
+      (new Set(
+        [
+          new Set(["1"])
+        ]
+      ))
   })
 
   test('Two individual nodes', () => {
     expect(getWeaklyConnectedComponents({
       1: [],
       2: []
-    })).toStrictEqual([])
-  })
+    })).toStrictEqual
+    (new Set(
+      [
+        new Set(["1"]),
+        new Set(["2"])
+      ]
+    ))
+})
 
   test('Two connected nodes (directed)', () => {
     expect(getWeaklyConnectedComponents({
-      1: [2],
+      1: ["2"],
       2: []
-    })).toStrictEqual([])
-  })
+    })).toStrictEqual
+    (new Set(
+      [
+        new Set(["1", "2"]),
+      ]
+    ))
+})
 
-  test('Two weakly connected components', () => {
+  test('Two weakly connected components (1)', () => {
     expect(getWeaklyConnectedComponents({
-      0: [1, 2],
-      1: [2],
+      0: ["1", "2"],
+      1: ["2"],
       2: [],
-      3: [4],
+      3: ["4"],
       4: []
-    })).toStrictEqual([])
-  })
+    })).toStrictEqual
+    (new Set(
+      [
+        new Set(["0", "1", "2"]),
+        new Set(["3", "4"])
+      ]
+    ))
+})
+
+test('Two weakly connected components (2)', () => {
+  expect(getWeaklyConnectedComponents({
+    0: ["1", "2"],
+    1: ["2", "5"],
+    2: [],
+    3: ["4"],
+    4: [],
+    5: []
+  })).toStrictEqual
+  (new Set(
+    [
+      new Set(["0", "1", "2", "5"]),
+      new Set(["3", "4"])
+    ]
+  ))
+})
+
+test('Two weakly connected components (3)', () => {
+  expect(getWeaklyConnectedComponents({
+    0: ["1", "2"],
+    1: ["2", "5", "6"],
+    2: [],
+    3: ["4"],
+    4: [],
+    5: [],
+    6: []
+  })).toStrictEqual
+  (new Set(
+    [
+      new Set(["0", "1", "2", "5", "6"]),
+      new Set(["3", "4"])
+    ]
+  ))
+})
+
+test('Two weakly connected components (4)', () => {
+  expect(getWeaklyConnectedComponents({
+    0: ["1", "2"],
+    1: ["5", "6"],
+    2: [],
+    3: ["4"],
+    4: [],
+    5: [],
+    6: []
+  })).toStrictEqual
+  (new Set(
+    [
+      new Set(["0", "1", "2", "5", "6"]),
+      new Set(["3", "4"])
+    ]
+  ))
+})
+
+test('Three weakly connected components', () => {
+  expect(getWeaklyConnectedComponents({
+    0: ["1", "2"],
+    1: ["5", "6"],
+    2: [],
+    3: ["4"],
+    4: [],
+    5: [],
+    6: [],
+    7: []
+  })).toStrictEqual
+  (new Set(
+    [
+      new Set(["0", "1", "2", "5", "6"]),
+      new Set(["3", "4"]),
+      new Set(["7"])
+    ]
+  ))
+})
